19c53f21bad7525eb6efc933f8d1190682496bba
lowkey <lowkey@asgard>
2013-11-06 14:20:37 +0100

    + game-main

---
+++ core/src/main/java/de/secondsystem/game01/impl/game/MainGameState.java
@@ -0,0 +1,27 @@
+package de.secondsystem.game01.impl.game;
+
+import de.secondsystem.game01.impl.GameContext;
+import de.secondsystem.game01.impl.GameState;
+
+public class MainGameState extends GameState {
+
+	public MainGameState( String mapId ) {
+		// TODO
+	}
+	
+	@Override
+	protected void onStart(GameContext ctx) {
+		// TODO: load map, save game, etc.
+	}
+
+	@Override
+	protected void onStop(GameContext ctx) {
+		// TODO: free resources
+	}
+
+	@Override
+	protected void onFrame(GameContext ctx) {
+		// TODO: game main-loop
+	}
+
+}
