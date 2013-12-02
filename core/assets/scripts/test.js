f775b14d5e158ce73e54ee516e77307fd82bd0e1
lowkey <lowkey@asgard>
2013-12-02 12:25:50 +0100

    Changed the Timer-Callback to "a better solution than args[0], args[1], ..."

--- core/assets/scripts/test.js
+++ core/assets/scripts/test.js
@@ -8,11 +8,6 @@ function touchFunc(entity, other) {
 	entity.jump();
 }
 
-for( var i=0; i<40; i++ ) {
-	for( var j=0; j<2; j++ ) {
-		entities.createControllable( "enemy", {"x": 450+i*80, "y": j*-200 } );
-	}
-}
     
 function timerFunc(timer, entity, arg1, arg2) {  
 	entity.jump();
@@ -31,3 +26,7 @@ function anotherTimerFunc(timer) {
 
 var entity = entities.createControllable( "enemy", {"x": 150, "y": 200, "events": {"TOUCHED": "touchFunc"} } );
 map.getTimerManager().createTimer(1000, true, "timerFunc", entity, " test1 ", " test2 ");
+
+var movingPlatform = entities.createMovingPlatform( "moving platform", {"x": 150, "y": 100, "repeated":true} );
+movingPlatform.addTargetPoint(300, 100);
+movingPlatform.addTargetPoint(150, 100);
