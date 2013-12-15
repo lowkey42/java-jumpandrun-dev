deca511040847591ecc6cd554b4502bb65c4a3f0
lowkey <lowkey@midgard>
2013-12-15 01:09:32 +0100

    Gamepad-Controll (first test)

--- core/src/main/java/de/secondsystem/game01/impl/game/controller/KeyboardController.java
+++ core/src/main/java/de/secondsystem/game01/impl/game/controller/KeyboardController.java
@@ -1,6 +1,8 @@
 package de.secondsystem.game01.impl.game.controller;
 
+import org.jsfml.window.Joystick;
 import org.jsfml.window.Keyboard;
+import org.jsfml.window.Joystick.Axis;
 import org.jsfml.window.Keyboard.Key;
 import org.jsfml.window.event.Event;
 
@@ -17,7 +19,44 @@ public final class KeyboardController extends AbstractGameEntityController {
 		this.mapping = mapping;
 	}
 	
+	boolean switchW_cooldown = false;
+	private static final int JOYSTICK_DZONE = 20;
+	private static final int JOYSTICK_AMAX = 100-JOYSTICK_DZONE;
+	
 	public void process() {
+		if( Joystick.isConnected(0) ) {
+			float x = Joystick.getAxisPosition(0, Axis.X);
+			float y = Joystick.getAxisPosition(0, Axis.Y);
+			
+			if(Joystick.isButtonPressed(0, 3))
+				System.out.println("X="+x+"; Y="+y+"; Z="+Joystick.getAxisPosition(0, Axis.Z)+"; V="+Joystick.getAxisPosition(0, Axis.V));
+			
+			if( x<-JOYSTICK_DZONE )
+				proxy.moveHorizontally(HDirection.LEFT, (-x-JOYSTICK_DZONE)/JOYSTICK_AMAX);
+			else if( x>JOYSTICK_DZONE )
+				proxy.moveHorizontally(HDirection.RIGHT, (x-JOYSTICK_DZONE)/JOYSTICK_AMAX);
+			
+			if( y<-JOYSTICK_DZONE )
+				proxy.moveVertically(VDirection.UP, (-y-JOYSTICK_DZONE)/JOYSTICK_AMAX);
+			else if( y>JOYSTICK_DZONE )
+				proxy.moveVertically(VDirection.DOWN, (y-JOYSTICK_DZONE)/JOYSTICK_AMAX);
+			
+			if( Joystick.isButtonPressed(0, 0) )
+				proxy.jump();
+
+			if( Joystick.isButtonPressed(0, 1) )
+				proxy.use();
+
+			if( Joystick.getAxisPosition(0, Axis.Z)>=50 ) {
+				if( !switchW_cooldown )
+					proxy.switchWorlds();
+				
+				switchW_cooldown = true;
+				
+			} else
+				switchW_cooldown = false;
+		}
+		
 		if( Keyboard.isKeyPressed(mapping.moveLeft) )
 			proxy.moveHorizontally(HDirection.LEFT, 1);
 		else if( Keyboard.isKeyPressed(mapping.moveRight) )
